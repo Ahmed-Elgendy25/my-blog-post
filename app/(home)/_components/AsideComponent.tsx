2a6cfb993d238be09f22c729d245615867962212
Ahmed-Elgendy25 <202123713@o6u.edu.eg>
2025-10-16 20:10:45 +0300

    Update AsideComponent image and import asset
    Replaced the static image path in AsideComponent with an imported local image asset (computer.jpg) for improved asset management. Added computer.jpg to the public directory and updated the alt text for clarity.

--- app/(home)/_components/AsideComponent.tsx
+++ app/(home)/_components/AsideComponent.tsx
@@ -1,33 +1,31 @@
-import Image from "next/image"
+import Image from "next/image";
+import computer from "/public/computer.jpg";
 
 function AsideComponent() {
   return (
     <aside className="   ">
-    <header className="my-6">
-      <h3 className=" text-lg font-bold  ">STACK STORIES MAGAZINE</h3>
-      <h2 className="text-6xl  font-bold">04/2025</h2>
-    </header>
+      <header className="my-6">
+        <h3 className=" text-lg font-bold  ">STACK STORIES MAGAZINE</h3>
+        <h2 className="text-6xl  font-bold">04/2025</h2>
+      </header>
 
-    <figure className="relative  ">
-      <Image
-        className="brightness-75"
-        src="/90's desktop.jpg"
-        width={500}
-        height={250}
-        alt="90's Desktop"
-      />
-      <figcaption className="absolute flex justify-center left-4   top-1/2 text-[#e7e8e2] ">
-        <p className=" font-bold text-7xl tracking-[13]  ">
-          STACK
-          <span className="text-3xl block tracking-normal">
-            STORIES
-          </span>
-        </p>
-      </figcaption>
-    </figure>
-
-</aside>
-  )
+      <figure className="relative  ">
+        <Image
+          className="brightness-75"
+          src={computer}
+          width={500}
+          height={250}
+          alt="90's Computer Desktop"
+        />
+        <figcaption className="absolute flex justify-center left-4   top-1/2 text-[#e7e8e2] ">
+          <p className=" font-bold text-7xl tracking-[13]  ">
+            STACK
+            <span className="text-3xl block tracking-normal">STORIES</span>
+          </p>
+        </figcaption>
+      </figure>
+    </aside>
+  );
 }
 
-export default AsideComponent
+export default AsideComponent;
