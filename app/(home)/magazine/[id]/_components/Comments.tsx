b5fd4e45d4e078516b55fdf27821abc9d9494bbe
Ahmed-Elgendy25 <ahmedashrafelgendy25@gmail.com>
2025-11-29 08:00:20 +0200

    Fixed Giscus Comments, The Error was about attribute values weren't configured well

--- app/(home)/magazine/[id]/_components/Comments.tsx
+++ app/(home)/magazine/[id]/_components/Comments.tsx
@@ -2,20 +2,20 @@
 import Giscus from "@giscus/react";
 
 export default function Comments() {
-    return (
-        <Giscus
-            repo="Ahmed-Elgendy25/my-blog-post"
-            repoId="R_kgDOOG8GpQ"
-            category="[ENTER CATEGORY NAME HERE]"
-            categoryId="[ENTER CATEGORY ID HERE]"
-            mapping="title"
-            strict="0"
-            reactionsEnabled="1"
-            emitMetadata="0"
-            inputPosition="bottom"
-            theme="light"
-            lang="en"
-            loading="lazy"
-        />
-    );
+  return (
+    <Giscus
+      repo="Ahmed-Elgendy25/Stack-Stories"
+      repoId="R_kgDOOG8GpQ"
+      category="General"
+      categoryId="DIC_kwDOOG8Gpc4CwNxG"
+      mapping="pathname"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="light"
+      lang="en"
+      loading="lazy"
+    />
+  );
 }
